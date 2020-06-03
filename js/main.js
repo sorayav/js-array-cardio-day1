21755c5f89496c869148261d733e680b097dda38
Soraya Valle <svalleortega@gmail.com>
2020-06-03 11:49:35 +0200

    Resolve first exercises

--- js/main.js
+++ js/main.js
@@ -20,18 +20,28 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
-
+const inventors1500 = inventors.filter(inventor => inventor.year >= 1500 && inventor.year <= 1599);
+//console.table(inventors1500);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
 
+const inventorsNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+//console.log(inventorsNames);
+
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
+const inventorsSortBirthdate = inventors.sort((a, b) => a.year - b.year);
+//console.table(inventorsSortBirthdate);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live all together?
+const inventorsYearsLived = inventors.reduce((acc, number) => acc + number.passed - number.year, 0);
+//console.log(inventorsYearsLived);
 
 // 5. Sort the inventors by years lived
+const inventorsSortByYears = inventors.sort((a, b) => (b.passed - b.year) - (a.passed - a.year));
+console.table(inventorsSortByYears);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
